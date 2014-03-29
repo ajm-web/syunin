05866e0462a6b45c72187b6697db37d483434755
Suguru FUJISAWA <udumge@gmail.com>
2014-03-29 23:32:13 +0900

    add test

--- PasswordCheck/in/plam/ajm/syunin/test/PasswordCheckerTest.java
+++ PasswordCheck/in/plam/ajm/syunin/test/PasswordCheckerTest.java
@@ -9,9 +9,40 @@ import static org.junit.Assert.*;
  * Created by mpon on 2014/03/29.
  */
 public class PasswordCheckerTest {
+    
     @Test
-    public void testCheck() throws Exception {
+    public void testLength() throws Exception {
         PasswordChecker pc = new PasswordChecker();
-        assertEquals(pc.check(""),true);
+        assertEquals(pc.check("A1213pokl"),false);
+    }
+    
+    @Test
+    public void testLengthOK() throws Exception {
+        PasswordChecker pc = new PasswordChecker();
+        assertEquals(pc.check("bAse730onE"),true);
+    }
+    
+    @Test
+    public void testNotDigitUpper() throws Exception {
+        PasswordChecker pc = new PasswordChecker();
+        assertEquals(pc.check("asasasasasasasaas"),false);
+    }
+    
+    @Test
+    public void testNotDigit() throws Exception {
+        PasswordChecker pc = new PasswordChecker();
+        assertEquals(pc.check("QWERTYqwerty"),false);
+    }
+    
+    @Test
+    public void testNotLetter() throws Exception {
+        PasswordChecker pc = new PasswordChecker();
+        assertEquals(pc.check("123456123456"),false);
+    }
+    
+    @Test
+    public void testNotLetterOK() throws Exception {
+        PasswordChecker pc = new PasswordChecker();
+        assertEquals(pc.check("QwErTy911poqqqq"),true);
     }
 }
