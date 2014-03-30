f2049c7666381810de10431e39e66d7df982dce7
Masato OSHIMA <qqw75pn9@gmail.com>
2014-03-30 10:16:11 +0900

    日付の比較方法が間違っていたので修正

--- Sunday/src/in/plam/ajm/Sunday.java
+++ Sunday/src/in/plam/ajm/Sunday.java
@@ -17,18 +17,18 @@ public class Sunday {
     public Integer calcSunday(){
         
         Calendar startDate = Calendar.getInstance();
-        startDate.set(1901, 1, 1);
+        startDate.set(1901, Calendar.JANUARY, 1);
         Calendar endDate = Calendar.getInstance();
-        endDate.set(2000, 12, 31);
+        endDate.set(2000, Calendar.DECEMBER, 31);
         
         int sundayCounter = 0;
         
-        while(!startDate.equals(endDate)){
-            
+        while(startDate.before(endDate)){
+
             if((startDate.get(Calendar.DATE) == 1) && (startDate.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) ){
                 sundayCounter+=1;
             }
-            
+
             startDate.add(Calendar.DATE, 1);
         }
         
